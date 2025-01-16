87386b900fe45ac960f33627ac8b11382bbcf007
tc <tc>
2025-01-17 00:01:49 +0100

    added responsive design to landing page

--- todo-app/frontend/src/pages/start.tsx
+++ todo-app/frontend/src/pages/start.tsx
@@ -2,16 +2,27 @@ import Logo from "../components/common/Logo";
 import NavLinks from "../components/layout/NavLinks";
 import AuthButtons from "../components/common/AuthButtons";
 
-function Start() {
+const Start = () => {
   return (
     <div className="min-h-screen bg-neutral-950">
-      <div className="flex items-center justify-center space-x-16 px-10 md:space-x-20 lg:space-x-[15%]">
-        <Logo />
-        <NavLinks />
-        <AuthButtons />
-      </div>
+      <header className="container mx-auto px-4 py-6">
+        <nav
+          className="flex flex-col items-center justify-between gap-6
+                        sm:flex-row sm:gap-8
+                        lg:gap-12"
+        >
+          <div className="w-[75px] h-[75px] flex-shrink-0">
+            <Logo />
+          </div>
+
+          <NavLinks />
+          <AuthButtons />
+        </nav>
+      </header>
+
+      <main className="container mx-auto px-4 py-8"></main>
     </div>
   );
-}
+};
 
 export default Start;
