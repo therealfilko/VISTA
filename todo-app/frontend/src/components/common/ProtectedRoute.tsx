81e0c83981e114bf89f5b9c8ad7c901586e01ec9
Tim Chmieletzki <s93207@bht-berlin.de>
2025-01-16 12:07:24 +0100

    updated ProtectedRoute.tsx, so login works again

--- todo-app/frontend/src/components/common/ProtectedRoute.tsx
+++ todo-app/frontend/src/components/common/ProtectedRoute.tsx
@@ -1,23 +1,17 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
-
 interface ProtectedRouteProps {
   children: JSX.Element;
 }
-
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const { isAuthenticated, token } = useAuth();
-
   if (token === null) {
     return <div>Laden...</div>;
   }
-
   if (!isAuthenticated || !token) {
     return <Navigate to="/login" />;
   }
-
   return children;
 };
-
 export default ProtectedRoute;
