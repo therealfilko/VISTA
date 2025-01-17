a1050bf75682cf649109bf3ddbfa4f2337aa5a64
TheRealFilko <fn.filipnikolic@gmail.com>
2025-01-18 00:04:44 +0100

    fix(fourth attempt): blank page localhost:3000

--- todo-app/frontend/vite.config.ts
+++ todo-app/frontend/vite.config.ts
@@ -4,23 +4,32 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
+    host: true,
     port: 3000,
     proxy: {
-      "/auth": "http://localhost:9000",
-      "/api": "http://localhost:9000",
+      "/auth": {
+        target: "http://app:9000",
+        changeOrigin: true
+      },
+      "/api": {
+        target: "http://app:9000",
+        changeOrigin: true
+      }
     },
     watch: {
       usePolling: true,
-    },
-    strictPort: true,
-    hmr: {
-      clientPort: 3000,
-      host: "0.0.0.0",
-    },
+    }
   },
   build: {
-    outDir: "dist",
     sourcemap: true,
-  },
+    manifest: true,
+    rollupOptions: {
+      output: {
+        format: 'es',
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]'
+      }
+    }
+  }
 });
