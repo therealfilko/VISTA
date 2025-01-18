477712f139b42cc882c450ef3563839c7b234127
TheRealFilko <fn.filipnikolic@gmail.com>
2025-01-18 12:18:02 +0100

    fix(blank): page on coolify still blank

--- todo-app/frontend/vite.config.ts
+++ todo-app/frontend/vite.config.ts
@@ -2,18 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
-    middlewareMode: false, // Stelle sicher, dass die Middleware korrekt deaktiviert ist
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  build: {
-    manifest: true,
-  },
+    plugins: [react()],
+    server: {
+        host: "0.0.0.0",
+        port: 3000,
+        strictPort: true,
+        hmr: {
+            host: "188.245.213.55",
+            port: 3000, 
+        }
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    build: {
+        manifest: true,
+    },
 });
 
